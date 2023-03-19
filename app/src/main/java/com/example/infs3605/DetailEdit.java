618e387d06b3d39ef69f1cc43411312bb6859994
YinghouQi <z5352204@ad.unsw.edu.au>
2023-03-19 21:41:35 +1100

    INFS3605group_vision0.02(login,menu.xml)

---
+++ app/src/main/java/com/example/infs3605/DetailEdit.java
@@ -0,0 +1,14 @@
+package com.example.infs3605;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_edit);
+    }
+}
