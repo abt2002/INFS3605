e559ff84cb979fde603ffe019243f9657d849ec3
abt2002 <z5312908@ad.unsw.edu.au>
2023-04-03 18:05:05 +1000

    Facebook login tokens

--- app/src/main/java/com/example/infs3605/API.java
+++ app/src/main/java/com/example/infs3605/API.java
@@ -2,5 +2,5 @@ package com.example.infs3605;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "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";
 }
